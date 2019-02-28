3daf4a04366c44c48d966ce6c0f4ac0691f4ba4f
SOYER Mathieu <mathieu.soyer@soprasteria.com>
2019-02-28 19:04:26 +0100

    a

---
+++ hashcode/Algo.py
@@ -0,0 +1,12 @@
+from Lanceur import Lanceur
+import copy
+import math
+import time
+import sys
+
+class Algo:
+    def __init__(self,lanceur):
+        print("fichier",fichier)
+
+    
+        self.lanceur = lanceur
