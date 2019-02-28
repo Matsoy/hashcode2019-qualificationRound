0d967836eb84364a8a4800304e0453af9ecb5a37
Legay Jordan <jordan.legay@soprasteria.com>
2019-02-28 19:27:03 +0100

    add slide common list of tags

--- hashcode/classes/Slide.py
+++ hashcode/classes/Slide.py
@@ -3,3 +3,7 @@ class Slide:
     def __init__(self,photo1,photo2):
         self.photo1 = photo1
         self.photo2 = photo2
+        if self.photo2:
+            self.tags = list(set(photo1.tagList+photo2.tagList))
+        else:
+            self.tags = photo1.tagList
